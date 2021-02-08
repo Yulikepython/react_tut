c99ad5f38a87f147b8e9432cba3864f64e8152e3
Yu <admin@itc.tokyo>
2021-02-08 23:17:18 +0900

    working on react tutorial Feb.8th

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,9 +3,13 @@ import React from "react"
 function Joke(props) {
     return (
         <div>
-            {props.question}
-            {props.punchLine}
-            
+            <p style={{display: props.question ? "block" : "none"}}>
+                Question:{props.question}
+            </p>
+            <p style={{display: props.punchLine ? "block" : "none"}}>
+                Answer:{props.punchLine}  
+            </p>
+            <hr/>
         </div>
     )
 }
