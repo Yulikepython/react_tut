7da74f2c016aa82e9e44208877aaa615a5003242
Yu <admin@itc.tokyo>
2021-02-15 00:18:28 +0900

    finished the coursegit add .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react"
 
 class App extends Component{
-    constructor(){
-        super()
-        this.state = {
 
-        }
-    }
+
 
     render(){
         return (
-            <div>almost last</div>
+            <div>
+
+            </div>
         )
     }
 }
